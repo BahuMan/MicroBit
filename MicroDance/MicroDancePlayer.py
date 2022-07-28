c12d2d6de3a19a4327af4fec6aa832e1a34088e1
Bart Huylebroeck <BahuMan@users.noreply.github.com>
2022-07-28 10:57:17 +0200

    Create MicroDancePlayer.py

---
+++ MicroDance/MicroDancePlayer.py
@@ -0,0 +1,40 @@
+from microbit import *
+import radio
+
+#accelerometer.set_range(2)
+radio.config(channel=54)
+radio.on()
+display.on()
+dx = -1
+dy = -1
+dz = -1
+streng = 200
+punten = 0
+
+def error(p):
+    display.scroll(p, wait=True, loop=True)
+    
+def vergelijk():
+    global dx, dy, dz, punten
+    if (dx < 0 or dy < 0 or dz < 0):
+        error("verkeerde volgorde van radio boodschappen")
+
+    if (dx < streng and dy < streng and dz < streng):
+        punten = punten + 1
+        dx = dy = dz = -1
+
+    
+while True:
+    msg = radio.receive()
+    if msg is None: continue
+    msg = msg.split(":")
+    if msg[0] == "x":
+        dx = abs(accelerometer.get_x() - int(msg[1]))
+    elif msg[0] == "y":
+        dy = abs(accelerometer.get_y() - int(msg[1]))
+    elif msg[0] == "z":
+        dz = abs(accelerometer.get_z() - int(msg[1]))
+        vergelijk()
+    else:
+        error("onbekende radio boodschap: " + msg[0])
+        
