e06fda7c0f80c9d3468cb72f5518597ce93ca353
Bart Huylebroeck <BahuMan@users.noreply.github.com>
2024-10-22 16:49:52 +0200

    Update kandidaat.py
    aanpassing voor unique_id()

--- quiz/kandidaat.py
+++ quiz/kandidaat.py
@@ -1,42 +1,46 @@
-# Add your Python code here. E.g.
 from microbit import *
 import radio
+from machine import unique_id
+import machine
 
-radio.config(channel=67, length=16)
+radio.config(channel=67, length=32)
 radio.on()
 
-radio.send("ikke")
-sleep(200)
-magmee = radio.receive()
+uniek = str(unique_id())[:6]
 
-if magmee is None:
-    while True:
-        display.scroll("quizmaster niet gevonden; reset om opnieuw te proberen")
-elif not magmee.startswith("jijbent"):
-    while True:
-        display.scroll("onbekend antwoord; reset om opnieuw te proberen")
-else:
-    vanalles = magmee.split()
-    laatste = vanalles[-1]
-    mijnnr = int(laatste)
+radio.send("req " + uniek)
+teamNummer = -1
 
-display.scroll("ik ben nr %d" % mijnnr, wait=False, loop=True)
+while teamNummer < 0:
+    magmee = radio.receive()
+    if magmee is None:
+        display.scroll("wacht op quiz master", delay=75)
+    elif magmee.startswith("welkom "):
+        vanalles = magmee.split()
+        if vanalles[1] == uniek:
+            laatste = vanalles[3]
+            teamNummer = int(laatste)
+        else:
+            display.scroll("ander team %s" % vanalles[1], delay=75)
+
+display.scroll("team %d" % teamNummer, wait=False, loop=True)
 eerste = -1
 while True:
     boodschap = radio.receive()
     if boodschap is None:
+        pass
+    elif boodschap.startswith("welkom"):
         pass
     elif boodschap == "ikstelvraag":
         eerste=-1
         display.scroll("?", wait=False, loop=True)
     elif boodschap.startswith("eerste"):
         eerste = int(boodschap.split()[-1])
-        if (mijnnr == eerste):
+        if (teamNummer == eerste):
             display.show([Image.HEART, Image.HAPPY, Image.SMILE], wait=False, loop=True)
         else:
             display.show(Image.ALL_CLOCKS, wait=False, loop=True)
     if eerste < 0 and (button_a.was_pressed() or button_b.was_pressed()):
         display.scroll("buzzz", wait=False, loop=True)
-        radio.send("buzz %d" % mijnnr)
+        radio.send("buzz %d" % teamNummer)
     sleep(50)
-
